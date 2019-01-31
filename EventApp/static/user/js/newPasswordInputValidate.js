600110a5a0d9f1e617f63df56ce703997e40fcee
Sanul-Raskar <sanulraskar@gmail.com>
2019-01-31 20:09:17 +0530

    Removed Email validation on NewPassword Page

--- EventApp/static/user/js/newPasswordInputValidate.js
+++ EventApp/static/user/js/newPasswordInputValidate.js
@@ -1,26 +1,6 @@
-var EMail = document.getElementById('EMail');
 var password1 = document.getElementById('password1');
 var password2 = document.getElementById('password2');
-var Email = 0, pass1 = 0, pass2 = 0;
-
-function validateEMail() {
-    var exp = /^([0-9a-zA-Z]([-.\w]*[0-9,a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-
-    if (EMail.value == "") {
-        document.getElementById("EmailErrors").style.display = "block";
-        document.getElementById("EmailErrors").innerHTML = "Required";
-        return false;
-    }
-
-    if (EMail.value.match(exp)) {
-        Email = 1;
-        return true;
-    } else {
-        document.getElementById("EmailErrors").style.display = "block";
-        document.getElementById("EmailErrors").innerHTML = "Invalid Email";
-        return false;
-    }
-}
+var  pass1 = 0, pass2 = 0;
 
 function validatePassword1() {
 
@@ -54,11 +34,10 @@ function validatePassword2() {
 
 function validate() {
     clearFields();
-    validateEMail();
     validatePassword1();
     validatePassword2();
 
-    if (Email && pass1 && pass2) {
+    if (pass1 && pass2) {
         return true;
     }
     else {
@@ -67,10 +46,8 @@ function validate() {
 }
 
 function clearFields() {
-    document.getElementById("EmailErrors").innerHTML = "";
     document.getElementById("password1Errors").innerHTML = "";
     document.getElementById("password2Errors").innerHTML = "";
-    document.getElementById("EmailErrors").style.display = "none";
     document.getElementById("password1Errors").style.display = "none";
     document.getElementById("password2Errors").style.display = "none";
 }
