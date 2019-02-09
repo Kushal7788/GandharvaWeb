813f8116cfd8c036bbadc6eedb98a210369827d7
Sanul-Raskar <sanulraskar@gmail.com>
2019-02-10 00:03:16 +0530

    Added item class to Owl

--- EventApp/static/base/js/base.js
+++ EventApp/static/base/js/base.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // Transition effect for navbar
     $(window).scroll(function () {
         // checks if window is scrolled more than 500px, adds/removes solid class
-        if ($(this).scrollTop() > 300) {
+        if ($(this).scrollTop() > 250) {
             $('.navbar').addClass('solid');
             $('.navbar').css("background-color: #004E8A;");
         } else {
