f8a00c9909acfb96b39b7c8d57399e35a272ad06
Unknown <jakash1997@gmail.com>
2019-02-12 20:51:21 +0530

    Updated to only store offline page

--- EventApp/templates/service-worker.js
+++ EventApp/templates/service-worker.js
@@ -1,41 +1,20 @@
-//This is the "Offline copy of pages" service worker
+// This is the "Offline copy of pages" service worker
 
-//Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener('install', function (event) {
-    self.skipWaiting();
-    var indexPage = new Request('/');
-    event.waitUntil(
-        fetch(indexPage).then(function (response) {
-            return caches.open('gandharva-offline').then(function (cache) {
-                console.log('Gandharva Cached index page during Install' + response.url);
-                return cache.put(indexPage, response);
-            });
-        }));
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', event => {
+  const offlinePage = new Request('/offline');
+  event.waitUntil(
+    fetch(offlinePage).then(response =>
+      caches.open('gandharva-offline-v1').then(cache => cache.put(offlinePage, response))
+    )
+  );
 });
 
-//If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener('fetch', function (event) {
-    var updateCache = function (request) {
-        return caches.open('gandharva-offline').then(function (cache) {
-            return fetch(request).then(function (response) {
-                return cache.put(request, response);
-            });
-        });
-    };
-
-    event.waitUntil(updateCache(event.request));
-
-    event.respondWith(
-        fetch(event.request).catch(function (error) {
-            //Check to see if you have it in the cache
-            //Return response
-            //If not in the cache, then return error page
-            return caches.open('gandharva-offline').then(function (cache) {
-                return cache.match(event.request).then(function (matching) {
-                    var report = !matching || matching.status == 404 ? Promise.reject('no-match') : matching;
-                    return report
-                });
-            });
-        })
-    );
-})
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.open('gandharva-offline-v1').then(cache => cache.match('/offline'))
+    )
+  );
+});
