96301b96cdd4898b1d841ab3e02de23f977c70cf
rahul <rshahshah2890@gmail.com>
2019-02-13 21:50:30 +0530

    User roles

--- GandharvaWeb19/settings.py
+++ GandharvaWeb19/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^)ef3%8r$&327z%qz92yxgcxt6m@4s8j7$czx%5r6140^zh&g4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'gandharva19.pythonanywhere.com', '0.0.0.0', 'localhost', 'www.viitgandharva.com',
                  'viitgandharva.com', '*.viitgandharva.com', '192.168.43.139', '192.168.43.211']
@@ -130,7 +130,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "EventApp/static")
@@ -147,7 +146,10 @@ STATIC_URL = '/static/'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '941888329527-bknhunrj63gk5id2ibhtd1ou4hv4bkc7.apps.googleusercontent.com'  # Paste CLient Key
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'FSqq7HvDDkZucHi0Wr-EHVuu'  # Paste Secret Key
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
@@ -158,10 +160,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_USER = 'viitgandharva3@gmail.com'
 
 
-# EMAIL_HOST_PASSWORD = 'GandharvaViitPune@19'
+# EMAIL_HOST_PASSWORD = 'GandharvaViitPune@19'http://127.0.0.1:8000/
 EMAIL_PORT = 587
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
 
 USE_HTTPS = False
