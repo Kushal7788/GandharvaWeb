315ecccfb00d7264d4b034c968351e93da968f5e
aanshi <aanshiverma03@gmail.com>
2019-02-13 20:43:00 +0530

    Checkbox added in participant details

--- GandharvaWeb19/settings.py
+++ GandharvaWeb19/settings.py
@@ -155,10 +155,10 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'viitgandharva3@gmail.com'
+# EMAIL_HOST_USER = 'viitgandharva3@gmail.com'
 
 
-EMAIL_HOST_PASSWORD = 'GandharvaViitPune@19'
+# EMAIL_HOST_PASSWORD = 'GandharvaViitPune@19'
 EMAIL_PORT = 587
 
 MEDIA_URL = '/media/'
