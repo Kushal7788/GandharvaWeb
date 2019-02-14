4556b9a977db09679dd7407534d1ccf7ff5eebc9
Manthan Agrawal <agrawalmanthan0@gmai.com>
2019-02-15 00:33:44 +0530

    Added GLOBAL EVENTS

--- EventApp/context_processors.py
+++ EventApp/context_processors.py
@@ -3,8 +3,15 @@ from .models import *
 
 def add_variable_to_context(request):
     obj = Document_type.objects.get(type='Rule Book')
+    global_objects = EventDepartment.objects.filter(department=6)
+    event_name = []
+    for global_object in global_objects:
+        event_name.append(global_object.event.event_name)
+
+
     return {
         'testmes': Department.objects.all(),
+        'global_events': event_name,
         'footers': SocialMedia.objects.all(),
         'rulebook': Document.objects.get(category = obj)
     }
