470f5d43c8bae49f37364265aa1ff293febcd1d9
akzarma <akzarma@gmail.com>
2019-02-28 20:21:19 +0530

    str change models team

--- EventApp/models.py
+++ EventApp/models.py
@@ -258,7 +258,7 @@ class Team(models.Model):
                                  null=True)
     ispresent = models.BooleanField(default=False)
     def __str__(self):
-        return str(self.user)
+        return str(self.user) + ' Referral: ' + str(self.referral)
 
 
 class Transaction(models.Model):
